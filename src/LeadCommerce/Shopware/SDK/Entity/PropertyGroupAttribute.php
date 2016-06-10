004fcc3a86a4e8fd680220f62a3a4b0e98744f5b
Alexander Mahrt <hello@youngandcreative.de>
2016-06-10 22:30:54 +0200

    #2 * PropertyGroupAttribute has now getters and setters.

--- src/LeadCommerce/Shopware/SDK/Entity/PropertyGroupAttribute.php
+++ src/LeadCommerce/Shopware/SDK/Entity/PropertyGroupAttribute.php
@@ -11,7 +11,54 @@
 namespace LeadCommerce\Shopware\SDK\Entity;
 
 
+/**
+ * Class PropertyGroupAttribute
+ * @package LeadCommerce\Shopware\SDK\Entity
+ */
 class PropertyGroupAttribute extends Base
 {
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var int
+     */
+    protected $propertyGroupId;
 
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return PropertyGroupAttribute
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPropertyGroupId()
+    {
+        return $this->propertyGroupId;
+    }
+
+    /**
+     * @param int $propertyGroupId
+     * @return PropertyGroupAttribute
+     */
+    public function setPropertyGroupId($propertyGroupId)
+    {
+        $this->propertyGroupId = $propertyGroupId;
+        return $this;
+    }
 }
