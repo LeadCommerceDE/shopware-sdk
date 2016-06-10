6d63c43dd73a1e014b321106545225debb3d225c
Alexander Mahrt <hello@youngandcreative.de>
2016-06-10 22:30:19 +0200

    #2 * PropertyGroup has now getters and setters.

--- src/LeadCommerce/Shopware/SDK/Entity/PropertyGroup.php
+++ src/LeadCommerce/Shopware/SDK/Entity/PropertyGroup.php
@@ -11,7 +11,120 @@
 namespace LeadCommerce\Shopware\SDK\Entity;
 
 
+/**
+ * Class PropertyGroup
+ * @package LeadCommerce\Shopware\SDK\Entity
+ */
 class PropertyGroup extends Base
 {
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var string
+     */
+    protected $name;
+    /**
+     * @var int
+     */
+    protected $position;
+    /**
+     * @var bool
+     */
+    protected $comparable;
+    /**
+     * @var int
+     */
+    protected $sortMode;
 
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return PropertyGroup
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return PropertyGroup
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param int $position
+     * @return PropertyGroup
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+        return $this;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isComparable()
+    {
+        return $this->comparable;
+    }
+
+    /**
+     * @param boolean $comparable
+     * @return PropertyGroup
+     */
+    public function setComparable($comparable)
+    {
+        $this->comparable = $comparable;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSortMode()
+    {
+        return $this->sortMode;
+    }
+
+    /**
+     * @param int $sortMode
+     * @return PropertyGroup
+     */
+    public function setSortMode($sortMode)
+    {
+        $this->sortMode = $sortMode;
+        return $this;
+    }
 }
