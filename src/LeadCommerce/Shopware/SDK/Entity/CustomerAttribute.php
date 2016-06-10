4248ad8573365b1ca3ccad51569cc84f2efcb11a
Alexander Mahrt <hello@youngandcreative.de>
2016-06-10 22:04:22 +0200

    #2 * CustomerAttribute has now getters and setters.

--- src/LeadCommerce/Shopware/SDK/Entity/CustomerAttribute.php
+++ src/LeadCommerce/Shopware/SDK/Entity/CustomerAttribute.php
@@ -11,7 +11,54 @@
 namespace LeadCommerce\Shopware\SDK\Entity;
 
 
+/**
+ * Class CustomerAttribute
+ * @package LeadCommerce\Shopware\SDK\Entity
+ */
 class CustomerAttribute extends Base
 {
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var int
+     */
+    protected $customerId;
 
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return CustomerAttribute
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCustomerId()
+    {
+        return $this->customerId;
+    }
+
+    /**
+     * @param int $customerId
+     * @return CustomerAttribute
+     */
+    public function setCustomerId($customerId)
+    {
+        $this->customerId = $customerId;
+        return $this;
+    }
 }
