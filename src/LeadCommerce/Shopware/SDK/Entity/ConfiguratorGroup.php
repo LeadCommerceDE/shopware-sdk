0507513ecf2989c4bea466e88fcbd7d2ee3495cc
Alexander Mahrt <hello@youngandcreative.de>
2016-06-10 21:58:27 +0200

    #2 * ConfiguratorGroup has now getters and setters.

--- src/LeadCommerce/Shopware/SDK/Entity/ConfiguratorGroup.php
+++ src/LeadCommerce/Shopware/SDK/Entity/ConfiguratorGroup.php
@@ -11,7 +11,98 @@
 namespace LeadCommerce\Shopware\SDK\Entity;
 
 
+/**
+ * Class ConfiguratorGroup
+ * @package LeadCommerce\Shopware\SDK\Entity
+ */
 class ConfiguratorGroup extends Base
 {
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var string
+     */
+    protected $description;
+    /**
+     * @var string
+     */
+    protected $name;
+    /**
+     * @var int
+     */
+    protected $position;
 
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return ConfiguratorGroup
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return ConfiguratorGroup
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return ConfiguratorGroup
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * @param int $position
+     * @return ConfiguratorGroup
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+        return $this;
+    }
 }
