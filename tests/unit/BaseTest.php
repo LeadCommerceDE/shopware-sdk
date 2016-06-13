e7da52e6f22204948af4271f2ac4b1d029d77e35
Alexander Mahrt <alexander.mahrt@aboutyou.de>
2016-06-13 20:25:23 +0200

    First try for unit tests

---
+++ tests/unit/BaseTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright 2016 LeadCommerce
+ *
+ * @author Alexander Mahrt <amahrt@leadcommerce.de>
+ * @copyright 2016 LeadCommerce <amahrt@leadcommerce.de>
+ */
+abstract class BaseTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \LeadCommerce\Shopware\SDK\ShopwareClient
+     */
+    protected $mockClient = null;
+
+    /**
+     * @return \GuzzleHttp\Handler\MockHandler
+     */
+    protected abstract function getMockHandler();
+
+    /**
+     * @return \LeadCommerce\Shopware\SDK\ShopwareClient
+     */
+    public function getMockClient()
+    {
+        if (!$this->mockClient) {
+            $this->mockClient = new \LeadCommerce\Shopware\SDK\ShopwareClient('', 'test', 'test');
+            $mock = $this->getMockHandler();
+            $handler = \GuzzleHttp\HandlerStack::create($mock);
+            $client = new \GuzzleHttp\Client(['handler' => $handler, 'base_uri' => 'http://shopware-shop.dev/api/']);
+            $this->mockClient->setClient($client);
+        }
+
+        return $this->mockClient;
+    }
+}
---
+++ tests/unit/ShopwareClientTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Copyright 2016 LeadCommerce
+ *
+ * @author Alexander Mahrt <amahrt@leadcommerce.de>
+ * @copyright 2016 LeadCommerce <amahrt@leadcommerce.de>
+ */
+class ShopwareClientTest extends BaseTest
+{
+
+    public function testRequest()
+    {
+
+    }
+
+    public function testGetAddressQuery()
+    {
+
+    }
+
+    /**
+     * @return \GuzzleHttp\Handler\MockHandler
+     */
+    protected function getMockHandler()
+    {
+        // TODO: Implement getMockHandler() method.
+    }
+}
